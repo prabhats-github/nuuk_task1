4b5227ccf6ea2e5ebab000be771ec0d2696b41f4
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2023-12-30 12:07:44 +0530

    Add files via upload
    Html, Css and JavaScript is used in above project for practicing demo website....as per the instructions followed by our Infotech Boss...
    Thank You....

---
+++ index.js
@@ -0,0 +1,14 @@
+function show_form(){
+    let a=document.getElementById("getfreequote")
+    a.style.display='flex';
+}
+
+function none_1(){
+    let a=document.getElementById("getfreequote")
+    a.style.display='none';
+}
+
+function view1(){
+    let a=document.getElementsByClassName(".card")
+    a.style.display='flex';
+}
